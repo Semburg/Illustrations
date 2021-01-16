fc51185b8534b1a59f035a46b83459cfbcc8361f
Semburg <semburg.sb@gmail.com>
2021-01-16 17:37:36 +0100

    vers_1_0

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+function testD(){
+    console.log("Dark mode test")
+    document.body.style.background = "black"
+
+    // ?    WARUM  funktioniert body.style.color nicht auf alle Textblocke   ?
+    document.body.style.color = "white"  
+
+    document.getElementById("butti").style.backgroundColor = "#222"
+    document.getElementById("member").style.backgroundColor = "#555"
+
+
+
+    document.getElementById("test145").style.color = "white"
+    document.getElementById("test146").style.color = "white"
+    document.getElementById("test147").style.color = "white"
+    document.getElementById("test148").style.color = "white"
+    document.getElementById("test149").style.color = "white"
+    document.getElementById("test150").style.color = "white"
+    document.getElementById("test151").style.color = "white"
+    document.getElementById("test152").style.color = "white"
+    document.getElementById("test153").style.color = "white"
+    document.getElementById("test154").style.color = "white"
+    document.getElementById("test155").style.color = "white"
+    document.getElementById("test156").style.color = "white"
+    document.getElementById("test157").style.color = "white"
+
+
+
+
+
+    document.getElementsByTagName("P")
+
+    // document.getElementsByTagName("h1").style.color="white"
+    // return false
+    
+}
+
+
+function testL(){
+    console.log("Light mode test")
+
+
+}
