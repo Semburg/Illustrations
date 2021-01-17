abd7dee5ab7cf960548189f037a4296ef999e10c
Semburg <semburg.sb@gmail.com>
2021-01-17 13:31:17 +0100

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
 function testD(){
-    console.log("Dark mode test")
+    // !   console.log("Dark mode test")      
+    // !      helpd a lot!!    :) 
     document.body.style.background = "black"
 
-    // ?    WARUM  funktioniert body.style.color nicht auf alle Textblocke   ?
+
+    // ?    WARUM  funktioniert body.style.color nicht auf alle Textbloecke   ?
     document.body.style.color = "white"  
+    // ?  ------------------------------------------
 
     document.getElementById("butti").style.backgroundColor = "#222"
-    document.getElementById("member").style.backgroundColor = "#555"
+    document.getElementById("member").style.backgroundColor = "rgb(8,17,25)"
 
-
+    // *      Colorizing
 
     document.getElementById("test145").style.color = "white"
     document.getElementById("test146").style.color = "white"
@@ -23,21 +26,20 @@ function testD(){
     document.getElementById("test155").style.color = "white"
     document.getElementById("test156").style.color = "white"
     document.getElementById("test157").style.color = "white"
+    document.getElementById("test158").style.color = "white"
+    document.getElementById("test10").style.color = "grey"
 
+//  *    Images change
 
-
-
-
-    document.getElementsByTagName("P")
-
-    // document.getElementsByTagName("h1").style.color="white"
-    // return false
-    
+    document.getElementById("picture1").src = "assets/img/darkmode-img1.png"
+    document.getElementById("picture2").src = "assets/img/darkmode-img2.png"
+    document.getElementById("picture3").src = "assets/img/darkmode-img3.png"
+    document.getElementById("picture4").src = "assets/img/darkmode-img4.png"
+    document.getElementById("picture5").src = "assets/img/darkmode-img5.png"
+    document.getElementById("picture6").src = "assets/img/darkmode-img6.png"
 }
 
-
+// *      Switch back
 function testL(){
-    console.log("Light mode test")
-
-
+    //console.log("Light mode test")      
 }
